ef21998ce7289f7e77bfc5d715b3712c50ceeccf
巴哈姆特 <ttwings@126.com>
2022-08-17 17:06:39 +0800

    调整结构，MV分离

--- core/global/Sys.gd
+++ core/global/Sys.gd
@@ -6,17 +6,17 @@ const SAVE_PATH = "user://game.sav"
 ## COLORS  同时准备作为等级颜色。 
 const COLOR_RED = "#D04648"		## 炼虚 6
 const COLOR_ORANGE = "#FF9C2A"	## 化神 5
-const COLOR_YELLOW = "#FFCC29"	## 元婴 4
-const COLOR_GREEN = "#4CD964"	## 金丹 3
+const COLOR_GREEN = "#4CD964"	## 元婴 4
+const COLOR_YELLOW = "#FFCC29"	## 金丹 3
 const COLOR_CYAN = "#5AC8FA"	## 筑基 2
 const COLOR_BLUE = "#4B77BE"	## 练气 1
 const COLOR_PURPLE = "#8E44AD"  ## 任务品
 
 const COLOUR_LIGHT_BLUE = "#5AC8FA"
 const COLOUR_LIGHT_GREEN = "#4CD964"
-const COLOR_GRAY = "#95A5A6"
+const COLOR_GRAY = "#95A5A6"	## 受损 -1
 const COLOR_BLACK = "#34495E"
-const COLOR_WHITE = "#ECF0F1"
+const COLOR_WHITE = "#ECF0F1"	## 普通 0
 
 # Z index, used to layer of actor, fx, item, etc.
 const LAYER_DECAL = 0
